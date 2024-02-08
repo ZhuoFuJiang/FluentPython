8225971a0c7fb7e28a519fefb7b961f999275c8d
ZhuoFuJiang <1136634662@qq.com>
2024-02-08 14:59:24 +0800

    add chapter23

---
+++ Chapter23/bulkfood_v4b.py
@@ -0,0 +1,43 @@
+class Quantity:
+    __counter = 0
+
+    def __init__(self):
+        cls = self.__class__
+        name = cls.__name__
+        index = cls.__counter
+        cls.__counter += 1
+        self.storage_name = '_{}#{}'.format(name, index)
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        else:
+            return getattr(instance, self.storage_name)
+
+    def __set__(self, instance, value):
+        if value > 0:
+            setattr(instance, self.storage_name, value)
+        else:
+            raise ValueError("value must be > 0")
+
+
+class LineItem:
+    weight = Quantity()
+    price = Quantity()
+
+    def __init__(self, description, weight, price):
+        self.description = description
+        self.weight = weight
+        self.price = price
+
+    def subtotal(self):
+        return self.weight * self.price
+
+
+if __name__ == "__main__":
+    walnuts = LineItem('walnuts', 1, 10)
+    # print(vars(walnuts))
+    # print(dir(walnuts))
+    print(getattr(walnuts, 'weight'))
+    print(LineItem.weight)
+
